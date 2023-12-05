050f45edf34a690a2d5b0fa411ca6a7abc42daf3
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-12-05 14:02:21 +0100

    Replace dummy text with data

--- src/script.js
+++ src/script.js
@@ -1,15 +1,39 @@
 'use strict';
 
+const init = async function () {
+  const response = await fetch('src/data.json');
+  const data = await response.json();
+
+  const accordionsContainer = document.querySelector('.FAQs__questions');
+
+  const html = data.reduce((markup, question) => {
+    return (markup += `<li class="FAQs__accordion">
+            <div class="FAQs__accordion--question">
+              <h3 class="question">${question.question}</h3>
+              <span class="FAQs__accordion--btn btn"></span>
+            </div>
+            <div class="FAQs__accordion--answer">
+              <p class="answer">${question.answer}</p>
+            </div>
+          </li>`);
+  }, '');
+
+  accordionsContainer.insertAdjacentHTML('afterbegin', html);
+};
+
+init();
+
+//Event Listeners
 const FAQsContainer = document.querySelector('.FAQs');
 
 FAQsContainer.addEventListener('click', function (e) {
-  const accordion = e.target.closest('.FAQs__accordion');
-  const answer = accordion.querySelector('.FAQs__accordion--answer');
+  const accordionQuestion = e.target.closest('.FAQs__accordion--question');
+  if (!accordionQuestion) return;
 
-  if (!accordion) return;
-  accordion.classList.toggle('open');
+  const answer = accordionQuestion.nextElementSibling;
+  accordionQuestion.classList.toggle('open');
 
-  if (accordion.classList.contains('open')) {
+  if (accordionQuestion.classList.contains('open')) {
     answer.style.maxHeight = answer.scrollHeight + 'px';
   } else {
     answer.style.maxHeight = 0;
